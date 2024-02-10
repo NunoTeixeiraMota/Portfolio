cd3e143604b5f653649e89dc8ba892f52dd408e9
JoseTeixeira1200941 <1200941@isep.ipp.pt>
2024-02-10 19:21:33 +0000

    Fixed actions

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Portfolio/', // Replace <REPO> with your actual repository name
   plugins: [react()],
 })
