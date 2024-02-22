0b2b81ee78f94ef1bb0067fceae679dc82014430
NunoTeixeiraMota <jnmteixeira@gmail.com>
2024-02-22 22:51:22 +0000

    Fix UI Librari import

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
-import { ChakraProvider } from '@chakra-ui/react'
 
 
 ReactDOM.render(
   <Router>
     <React.StrictMode>
-      <ChakraProvider>
         <App />
-      </ChakraProvider>
     </React.StrictMode>
   </Router>
   ,
