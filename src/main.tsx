e7349fad464ab64803f28dfdb5278e8789e05e3c
NunoTeixeiraMota <zzzznunozzzz@gmail.com>
2024-06-17 14:52:20 +0100

    Analytics

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App.tsx';
 import './index.css';
+import { Analytics } from "@vercel/analytics/react"
 
 
 ReactDOM.render(
   <Router>
+    <Analytics />
     <React.StrictMode>
         <App />
     </React.StrictMode>
